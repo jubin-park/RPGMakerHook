be2ad9b24c765c9f862a72507786cbb1df9707f6
Jubin Park <jubin-park@naver.com>
2023-06-18 13:37:22 +0900

    Update Main.cpp

--- RPGMakerHook/Main.cpp
+++ RPGMakerHook/Main.cpp
@@ -51,6 +51,11 @@ int HookRPGXP(const wchar_t* const lpGameIniFilePath, const wchar_t* const lpRPG
 		return 1;
 	}
 	wprintf_s(L"HMODULE hRPGXPLibrary = %p\n", hRPGXPLibrary);
+	if (hRPGXPLibrary != (void*)0x00400000)
+	{
+		wprintf_s(L"hRPGXPLibrary must be %p\n", (void*)0x00400000);
+		return 1;
+	}
 
 	DWORD pidRPGXP = 0;
 	ret = GetWindowThreadProcessId(hRPGXPWindow, &pidRPGXP);
@@ -67,12 +72,14 @@ int HookRPGXP(const wchar_t* const lpGameIniFilePath, const wchar_t* const lpRPG
 		wprintf_s(L"OpenProcess() error = %d (%s:%d)\n", GetLastError(), _T(__FILE__), __LINE__);
 		return 1;
 	}
-	wprintf_s(L"HANDLE hProcess = %d\n", hProcess);
+	wprintf_s(L"HANDLE hProcess = %d\n", reinterpret_cast<int>(hProcess));
 
 	RPGXP::CommandInfo* pSaveCommand = &RPGXP::gCommands[RPGXP::eCommandType::DEFINE_AND_CALL_RPGXP_SAVE_WITHOUT_SCRIPTS];
 	SIZE_T scriptLength = strlen(pSaveCommand->Scripts);
 	DWORD oldProtect1 = 0;
 	VOID* pAddr = (VOID*)((char*)(hRPGXPLibrary) + pSaveCommand->Offset);
+
+	Sleep(1000);
 	ret = VirtualProtectEx(hProcess, pAddr, scriptLength, PAGE_EXECUTE_READWRITE, &oldProtect1);
 	{
 		if (ret == 0)
@@ -82,13 +89,14 @@ int HookRPGXP(const wchar_t* const lpGameIniFilePath, const wchar_t* const lpRPG
 		}
 
 		SIZE_T outSize = 0;
+		Sleep(1000);
 		ret = WriteProcessMemory(hProcess, pAddr, pSaveCommand->Scripts, scriptLength, &outSize);
 		if (ret == 0)
 		{
 			wprintf_s(L"1 WriteProcessMemory() error = %d (%s:%d)\n", GetLastError(), _T(__FILE__), __LINE__);
 			return 1;
 		}
-	}
+	}	
 
 	RPGXP::CommandInfo* pCallSaveCommand = &RPGXP::gCommands[RPGXP::eCommandType::CALL_RPGXP_SAVE];
 	scriptLength = strlen(pCallSaveCommand->Scripts);
@@ -96,6 +104,7 @@ int HookRPGXP(const wchar_t* const lpGameIniFilePath, const wchar_t* const lpRPG
 	VOID* pSaveAddr = (VOID*)((char*)(hRPGXPLibrary) + pCallSaveCommand->Offset);
 	char bytes[4] = { (int)pAddr & 0xff, ((int)pAddr >> 8) & 0xff, ((int)pAddr >> 16) & 0xff, ((int)pAddr >> 24) & 0xff };
 
+	Sleep(1000);
 	ret = VirtualProtectEx(hProcess, pSaveAddr, sizeof(bytes), PAGE_EXECUTE_READWRITE, &oldProtect2);
 	{
 		if (ret == 0)
@@ -104,6 +113,7 @@ int HookRPGXP(const wchar_t* const lpGameIniFilePath, const wchar_t* const lpRPG
 			return 1;
 		}
 		SIZE_T outSize = 0;
+		Sleep(1000);
 		ret = WriteProcessMemory(hProcess, pSaveAddr, bytes, sizeof(bytes), &outSize);
 		if (ret == 0)
 		{
